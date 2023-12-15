57ee109b3fe26772632a833fa570809b49bf96e8
Sarah Hoffmann <sarah.hoffann@hs-niederrhein.de>
2023-12-15 09:15:09 +0100

    update count_entries.sh

--- count_entries.sh
+++ count_entries.sh
@@ -1,5 +1,7 @@
 # A script to count all lines in a TSC file
-# 
+# Sarah Hoffmann
+# Datum: 2023-12-01
+
 echo "Counting the number of lines in all TSV files"
 wc -l *tsv
 echo "Done. Have a nice day."
